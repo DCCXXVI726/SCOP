004942785c311feaff5cc83ed176c65fb966e2b2
DCCXXVI726 <DCCXXVI726726@gmail.com>
2020-08-26 02:22:25 +0500

    s

--- src/main.c
+++ src/main.c
@@ -22,14 +22,20 @@ int		main(int ac, char **av)
 {
 	t_scop	*scop;
 
-	if (ac != 2)
+	if (ac != 3)
+	{
+		ft_putendl("Usage: ./scop [object] [texture]");
 		return (0);
+	}
 	scop = create_struct();
 	init_window(scop);
 	scop->shader_program = create_program();
 	scop->vve = create_vao_vbo(av[1]);
-	if (create_texture("resources/cat.png", &(scop->texture)) != 0)
+	if (create_texture(av[2], &(scop->texture)) != 0)
+	{
+		ft_putendl("problem with texture");
 		ft_error(0);
+	}
 	ft_loop(scop);
 	ft_clear(scop);
 	return (0);
